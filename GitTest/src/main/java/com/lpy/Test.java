aae92b549a3eab5b3e6f4f91065c11a038e47a1e
lpy14814 <1481450441@qq.com>
2020-07-11 09:18:11 +0800

    create test1

---
+++ GitTest/src/main/java/com/lpy/Test.java
@@ -0,0 +1,14 @@
+package com.lpy;
+
+/**
+ * @author lpy
+ * @create 2020-07-11-9:16
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("lpy111111111");
+
+    }
+}
